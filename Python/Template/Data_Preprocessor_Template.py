c335845bdb5d02015fa81f8329a217c010154fb6
abhimalamkar <abhijeetmalamkar@gmail.com>
2016-12-09 13:18:45 -0500

    Windows

--- Python/Template/Data_Preprocessor_Template.py
+++ Python/Template/Data_Preprocessor_Template.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
-
-#Data preprocessing 
-
-#Importing Libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,:-1].values
